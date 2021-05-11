e36774066675789d7cd8239ef74ddef1e8fb8718
avilash <avilashjit@gmail.com>
2021-05-11 13:10:47 +0530

    node project first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+//console.log(path.join(__dirname,'public'))
+const PATH = path.join(__dirname,'public');
+app.use(express.static(PATH));
+
+app.get('/',(req,res)=>{
+    res.send('node is running')
+})
+
+app.listen(3000);
